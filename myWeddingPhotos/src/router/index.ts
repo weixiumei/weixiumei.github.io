1dea1b63cf3b23374f9a0b669187bad4243925c0
weixiumei <weixiumei@nexttao.com>
2022-12-06 18:07:32 +0800

    dist

--- myWeddingPhotos/src/router/index.ts
+++ myWeddingPhotos/src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/photo",
+      path: "/",
       name: "photo",
       component: PhotosView,
     },
